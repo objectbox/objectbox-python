0927227e1cf05528f1bf262fbfaa08e91fb219eb
loryruta <lorenzo@objectbox.io>
2024-04-24 13:57:14 +0200

    find_ids: replace ulong (platform-dependant) with ulonglong (uint64) #33
    Fix arm7vhf/windows CI

--- objectbox/query.py
+++ objectbox/query.py
@@ -46,7 +46,7 @@ class Query:
         c_id_array_p = obx_query_find_ids(self._c_query)
         try:
             c_id_array: OBX_id_array = c_id_array_p.contents
-            ids = ctypes.cast(c_id_array.ids, ctypes.POINTER(ctypes.c_ulong * c_id_array.count))
+            ids = ctypes.cast(c_id_array.ids, ctypes.POINTER(ctypes.c_ulonglong * c_id_array.count))
             return list(ids.contents)
         finally:
             obx_id_array_free(c_id_array_p)
