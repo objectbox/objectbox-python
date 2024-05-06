6ab924d2388751867a9702444ba0f393a76e1e14
Daniel Adler <noreply+daniel@objectbox.io>
2024-05-06 18:17:28 +0200

    example/ollama: added #43

---
+++ example/ollama/README.md
@@ -0,0 +1,39 @@
+# Example: Using ObjectBox with Ollama 
+
+based on https://ollama.com/blog/embedding-models
+
+## Setup
+
+ 1. Install ollama. See instructions at https://ollama.com/download
+
+ 2. Pull models
+
+        ollama pull ollama2
+        ollama pull mxbai-embed-large
+
+ 3. Change to example directory:
+
+        cd example/ollama
+
+ 3. Recommended: Create a new venv
+
+        python3 -m venv .venv
+        source .venv/bin/activate
+
+ 4. Install Python Bindings and ObjectBox: 
+
+        pip install ollama
+        pip install --pre objectbox~=0.7.0a
+
+    Or: 
+
+        pip install -r requirements.txt
+
+
+## Run Example
+        
+```
+$ python llamas.py 
+
+Llamas are members of the camel family, which includes other large, even-toed ungulates such as camels, dromedaries, and Bactrian camels. Llamas are most closely related to alpacas, which are also native to South America and share many similarities in terms of their physical characteristics and behavior. Both llamas and alpacas belong to the family Camelidae, and are classified as ruminants due to their unique digestive system that allows them to break down cellulose in plant material.
+```
---
+++ example/ollama/llamas.py
@@ -0,0 +1,73 @@
+# Example based on https://ollama.com/blog/embedding-models
+# using objectbox as a vector store
+
+import ollama
+import objectbox
+
+documents = [
+  "Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels",
+  "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands",
+  "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall",
+  "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight",
+  "Llamas are vegetarians and have very efficient digestive systems",
+  "Llamas live to be about 20 years old, though some only live for 15 years and others live to be 30 years old",
+]
+
+
+from objectbox.model import *
+from objectbox.model.properties import *
+import numpy as np
+
+
+@Entity(id=1, uid=1)
+class DocumentEmbedding:
+    id = Id(id=1, uid=1001)
+    document = Property(str, id=2, uid=1002)
+    embedding = Property(np.ndarray, type=PropertyType.floatVector, id=3, uid=1003, index=HnswIndex(
+        id=3, uid=10001,
+        dimensions=1024,
+        distance_type=HnswDistanceType.EUCLIDEAN
+    ))
+
+model = Model()
+model.entity(DocumentEmbedding, last_property_id=IdUid(3, 1003))
+model.last_entity_id = IdUid(1, 1)
+model.last_index_id = IdUid(3,10001)
+
+ob = objectbox.Builder().model(model).build()
+box = objectbox.Box(ob, DocumentEmbedding)
+
+
+# store each document in a vector embedding database
+for i, d in enumerate(documents):
+  response = ollama.embeddings(model="mxbai-embed-large", prompt=d)
+  embedding = response["embedding"]
+
+  box.put(DocumentEmbedding(document=d,embedding=embedding))
+ 
+# an example prompt
+prompt = "What animals are llamas related to?"
+
+# generate an embedding for the prompt and retrieve the most relevant doc
+response = ollama.embeddings(
+  prompt=prompt,
+  model="mxbai-embed-large"
+)
+
+
+embedding_prop: Property = DocumentEmbedding.get_property("embedding")
+query = box.query(
+    embedding_prop.nearest_neighbor(response["embedding"], 1)
+).build()
+
+results = query.find_with_scores()
+data = results[0][0].document 
+
+
+# generate a response combining the prompt and data we retrieved in step 2
+output = ollama.generate(
+  model="llama2",
+  prompt=f"Using this data: {data}. Respond to this prompt: {prompt}"
+)
+
+print(output['response'])
