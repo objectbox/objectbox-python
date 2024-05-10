921cc54b4f7c5e93e29039a1159685dd47f0a277
Daniel Adler <noreply+daniel@objectbox.io>
2024-05-10 12:21:06 +0200

    test_inmemory: minor cleanup #44

--- tests/test_inmemory.py
+++ tests/test_inmemory.py
@@ -6,11 +6,9 @@ import shutil
 
 
 def test_inmemory():
-    # Expect path for persistent store
-    db_name = "testdata_persistent"
-    if os.path.exists(db_name):
-        shutil.rmtree(db_name) 
-    ob = load_empty_test_default_store(db_name)
+    # Use default path for persistent store
+    db_name = "testdata"
+    ob = load_empty_test_default_store()
     box = objectbox.Box(ob, TestEntity)
     object = TestEntity()
     id = box.put(object)
