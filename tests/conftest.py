204ba66c29fe4b075e1fc3ec9d3c97aa9bd23c5f
loryruta <lorenzo@objectbox.io>
2024-05-20 15:04:34 +0200

    tests: comment cleanup_db() (not needed) #25

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,14 @@
 import pytest
 from objectbox.logger import logger
 from common import *
-from objectbox.store import Store
 
 
 # Fixtures in this file are used by all files in the same directory:
 # https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
 
 
-def _clear_test_db():
-    Store.remove_db_files("testdata")
-
-
 @pytest.fixture(autouse=True)
 def cleanup_db():
-    """ Fixture to ensure tests starts fresh and the DB is cleaned up on success/failure. """
-    _clear_test_db()
-    try:
-        yield  # Run the test code
-    finally:
-        _clear_test_db()
+    # Not needed: every test clears the DB on start, without deleting it on exit (not necessary)
+    # Also, here we have no information regarding the DB path being used (although usually is "testdata")
+    pass
