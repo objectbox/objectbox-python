44a9470b9edd6c0d209b6a4f70c2e422de2b0603
Daniel Adler <noreply+daniel@objectbox.io>
2024-05-21 11:04:51 +0200

    Fix: test_deprecated explicitly sets directory #25

--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -6,11 +6,11 @@ from objectbox.c import *
 from objectbox.store_options import StoreOptions
 from tests.common import *
 
-
 def test_deprecated_ObjectBox():
     model = tests.common.create_default_model()
     options = StoreOptions()
     options.model(model)
+    options.directory("testdata")
     c_store = obx_store_open(options._c_handle)
     with pytest.deprecated_call():
         ob = ObjectBox(c_store)
