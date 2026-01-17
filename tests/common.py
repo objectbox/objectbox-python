0fc7774a2a3cd8fa3cd6de5f0dd5464c3963f60d
Shubham <noreply+shubham@objectbox.io>
2026-01-17 13:25:01 +0530

    Use typing.Union to avoid build failures in Python version < 3.10

--- tests/common.py
+++ tests/common.py
@@ -2,6 +2,7 @@ import os
 import time
 import subprocess
 import socket
+import typing
 
 import pytest
 import objectbox
@@ -51,7 +52,7 @@ class SyncServerConfig:
     port: int
 
 
-def start_sync_server() -> SyncServerConfig | None:
+def start_sync_server() -> typing.Union[SyncServerConfig, None]:
     """ Starts the ObjectBox Sync Server in a Docker container. """
     current_dir = os.path.dirname(os.path.realpath(__file__))
     user_id = os.getuid()
